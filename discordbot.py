e376a36c14505f84b9ce995ed30f5598d80a6817
shiotan107 <51515316+shiotan107@users.noreply.github.com>
2019-06-09 02:57:25 +0900

    shiobot,py

--- discordbot.py
+++ discordbot.py
@@ -12,8 +12,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def ohayo(ctx):
+    await ctx.send('ohayo')
 
 
 bot.run(token)
